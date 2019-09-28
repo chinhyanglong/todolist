adccc64b17930015cae65c5a4fd7be84aef8daa4
chinhyanglong <50737316+chinhyanglong@users.noreply.github.com>
2019-09-28 22:07:07 +0700

    1

--- src/App.js
+++ src/App.js
@@ -6,20 +6,51 @@ import TodoItem from './components/TodoItem';
 class App extends React.Component{
   constructor(){
     super();
-    this.todoItems = [
+    this.state= { 
+      todoItems : [
       {title: 'Đi mua bim bim', isComplete : true},
       {title: 'Đi chợ'},
       {title: 'Đi về'}
-    ] ;
+    ]
+   } 
    }
+   onItemClicked(item){
+     return (event)=> {
+       console.log(item);
+      const isComplete = item.isComplete;
+      const { todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+       this.setState({
+         todoItems : [
+           ...todoItems.slice(0,index),
+           {
+             ...item, isComplete: !isComplete
+           },
+           ...todoItems.slice(index + 1)
+         ]
+       })
+     }
+
+   }
+   
+
    render() {
+     const { todoItems } = this.state;
+     if(todoItems.length) {
     return (
       <div className="App">
       {
-        this.todoItems.map((item,index) => <TodoItem key={index} item = {item}/> )
+        todoItems.length && todoItems.map((item,index) =>
+         <TodoItem 
+            key={index} 
+            item = {item}
+            onClick = {this.onItemClicked(item)} /> 
+            )
       }
+      {todoItems.length === 0 && 'Nothing here!' }
       </div>
     );
+     }
     }
 }
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import './TodoItem.css';
+import classNames from 'classnames';
+import checkImg from '../img/check.svg';
+import checkedImg from '../img/checked.svg';
+
 class TodoItem extends Component{
     render() {
-        const { item } = this.props;
-        let className = 'TodoItem';
-
-        if(item.isComplete) {
-            className += ' TodoItem-complete';
+        const { item, onClick } = this.props;
+        let url = checkImg;
+        if(item.isComplete){
+            url = checkedImg;
         }
+
         return (
-        <div className= {className} >
+        <div  className= {classNames('TodoItem',{
+            'TodoItem-complete' : item.isComplete
+        })} >
+            <img onClick={onClick} src={url} width = {32} height = {32} />
                 <p>{this.props.item.title}</p>
         </div>
         );
